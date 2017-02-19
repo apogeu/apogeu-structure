49cbd0d4e43fa5250cf163183a0cde660c6c7c88
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-02-19 02:53:13 -0300

    pug and homepage

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 // config/routes.js
 
 module.exports = {
+  'get /': HomepagesController.index,
   'get /users': UsersController.list,
   'post /users': UsersController.create,
   'get /users/:id': UsersController.findById,
