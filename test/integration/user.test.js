164e6a63b3a2a7c9fd7bf4a39ffebe249712bbf6
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-03-13 23:01:04 -0300

    improved test integration

--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 
 it('list users', (done) => {
-  UsersService.list({ limit: 1 })
+  UsersService.list()
     .then((results) => {
-      assert.equal(results.length, 1);
+      assert.equal(Array.isArray(results), true);
       done();
     });
 });
